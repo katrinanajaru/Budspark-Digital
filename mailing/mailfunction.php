17f8410d4d479da48fa6f245256946e61327939a
katrinanajaru <najarulekalkuli@gmail.com>
2026-03-11 12:51:09 +0300

    Refine header link states and improve enquiry mail sending

--- mailing/mailfunction.php
+++ mailing/mailfunction.php
@@ -7,7 +7,7 @@ use PHPMailer\PHPMailer\Exception;
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/mailingvariables.php';
 
-function mailfunction($mail_reciever_email, $mail_reciever_name, $mail_msg, $attachment = false){
+function mailfunction($mail_recievers, $mail_msg, $plainTextMsg, $replyToEmail = '', $replyToName = '', $attachment = false){
     try {
         $mail = new PHPMailer(true);
         $mail->isSMTP();
@@ -20,13 +20,27 @@ function mailfunction($mail_reciever_email, $mail_reciever_name, $mail_msg, $att
         $mail->SMTPAuth = true;
         $mail->Username = $GLOBALS['mail_sender_email'];
         $mail->Password = $GLOBALS['mail_sender_password'];
+        $mail->CharSet = 'UTF-8';
 
         $mail->setFrom($GLOBALS['mail_sender_email'], $GLOBALS['mail_sender_name']);
-        $mail->addAddress($mail_reciever_email, $mail_reciever_name);
+
+        foreach ($mail_recievers as $recipient) {
+            $recipientEmail = $recipient['email'] ?? '';
+            $recipientName = $recipient['name'] ?? '';
+
+            if (!empty($recipientEmail)) {
+                $mail->addAddress($recipientEmail, $recipientName);
+            }
+        }
+
+        if (!empty($replyToEmail)) {
+            $mail->addReplyTo($replyToEmail, $replyToName);
+        }
+
         $mail->Subject = 'Someone Contacted You!';
         $mail->isHTML(true);
         $mail->msgHTML($mail_msg);
-        $mail->AltBody = 'This is a plain-text message body';
+        $mail->AltBody = $plainTextMsg;
 
         if ($attachment !== false) {
             $mail->addAttachment($attachment);
@@ -37,6 +51,14 @@ function mailfunction($mail_reciever_email, $mail_reciever_name, $mail_msg, $att
     } catch (Exception $e) {
         return array('success' => false, 'error' => $mail->ErrorInfo ?: $e->getMessage());
     }
+}
+
+function buildPlainTextMessage($name, $email, $phone, $project) {
+    return "New Consultation Booking\n"
+        . "Name: {$name}\n"
+        . "Email: {$email}\n"
+        . "Phone: {$phone}\n"
+        . "Project / Query: {$project}\n";
 }
 
 function isMailConfigReady() {
@@ -78,15 +100,19 @@ if (isset($_POST['submit'])) {
         $attachment = $_FILES['attachment']['tmp_name'];
     }
 
-    // Send the enquiry to both published contact emails
-    $sentToIan = mailfunction('iankinyua322@gmail.com', 'Ian Kinyua', $message, $attachment);
-    $sentToKatrina = mailfunction('katrinanajaru1@gmail.com', 'Katrina Najaru', $message, $attachment);
-    $sent = $sentToIan['success'] && $sentToKatrina['success'];
+    $recipients = array(
+        array('email' => 'iankinyua322@gmail.com', 'name' => 'Ian Kinyua'),
+        array('email' => 'katrinanajaru1@gmail.com', 'name' => 'Katrina Najaru'),
+    );
 
-    if ($sent) {
+    $plainTextMessage = buildPlainTextMessage($name, $email, $phone, $project);
+    $sendResult = mailfunction($recipients, $message, $plainTextMessage, $email, $name, $attachment);
+
+    if ($sendResult['success']) {
         echo "<script>alert('Thank you! Your consultation request has been sent successfully.'); window.location='../index.html';</script>";
     } else {
-        $errorMessage = $sentToIan['error'] ?: $sentToKatrina['error'] ?: 'Unknown mail error.';
+        error_log('BudSpark consultation mail error: ' . $sendResult['error']);
+        $errorMessage = $sendResult['error'] ?: 'Unknown mail error.';
         $safeErrorMessage = htmlspecialchars($errorMessage, ENT_QUOTES);
         echo "<script>alert('Mail error: {$safeErrorMessage}'); window.location='../index.html';</script>";
     }
