6f886611a97957eb2c9ca244e3318d5e313f48bc
katrinanajaru <najarulekalkuli@gmail.com>
2026-03-11 11:29:35 +0300

    First upload of BudSpark website

---
+++ js/main.js
@@ -0,0 +1,99 @@
+$(document).ready(function(){
+    function syncHeaderState() {
+        var isMobile = $(window).width() <= 1000;
+
+        if (!isMobile) {
+            $('.fa-bars').removeClass('fa-times');
+            $('.navbar').removeClass('nav-toggle').removeAttr('style');
+        }
+
+        if($(window).scrollTop()>35) {
+            $('.header').addClass('scrolled');
+        } else {
+            $('.header').removeClass('scrolled');
+        }
+    }
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+        syncHeaderState();
+    });
+
+    $(window).on('resize orientationchange', function () {
+        syncHeaderState();
+    });
+
+    const counters = document.querySelectorAll('.counter');
+    const speed = 120;
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+            const inc = target / speed;
+            if (count < target) {
+                counter.innerText = count + inc;
+                setTimeout(updateCount, 1);
+            } else {
+                counter.innerText = target;
+            }
+        };
+        updateCount();
+    });
+
+    (function ($) {
+        "use strict";
+
+        $(".clients-carousel").owlCarousel({
+            autoplay: true,
+            dots: true,
+            loop: true,
+            responsive: { 0: {items: 2}, 768: {items: 4}, 900: {items: 6} }
+        });
+
+        $(".testimonials-carousel").owlCarousel({
+            autoplay: true,
+            dots: true,
+            loop: true,
+            responsive: { 0: {items: 1}, 576: {items: 2}, 768: {items: 3}, 992: {items: 4} }
+        });
+
+    })(jQuery);
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    // ===== Accordion exclusive open =====
+    $('.accordion-header').click(function(){
+        var $accordionBody = $(this).next('.accordion-body');
+        var $span = $(this).children('span');
+
+        // Close all other open accordions
+        $('.accordion-body').not($accordionBody).slideUp(500);
+        $('.accordion-header').not(this).children('span').text('+');
+
+        // Toggle the clicked one
+        $accordionBody.slideToggle(500);
+        $span.text(function(_, currentText){
+            return currentText === '+' ? '-' : '+';
+        });
+    });
+
+    syncHeaderState();
+
+});
